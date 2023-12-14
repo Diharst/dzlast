52a762b6f2d63b1ca7e6c9365f3772e7e0145875
Dmitriy <kravchenkodv03@gmail.com>
2023-12-15 00:57:06 +0300

    dz

---
+++ dz1/Program.cs
@@ -0,0 +1,32 @@
+﻿﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+
+int input(string text)
+{
+    do{
+        Console.Write(text);
+        if (int.TryParse(Console.ReadLine(), out int index) && index >= 0)
+            return index;
+        else
+             Console.WriteLine("Некорректный ввод. Введите положительное целое число."); 
+    } while (true);
+}
+
+void show(int volumeM, int volumeN)
+{   
+    if (volumeM <= volumeN)
+    {
+        Console.Write(volumeM == volumeN ? volumeM : volumeM + ", ");
+        show(volumeM + 1, volumeN);
+    }
+}
+
+int m = input("Введите число M: ");
+int n = input("Введите число N: ");
+Console.Write($"M = {m}; N = {n} => \"");
+show(m, n);
+Console.WriteLine("\"");
---
+++ dz3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
+
+int input(string text)
+{
+    do{
+        Console.Write(text);
+        if (int.TryParse(Console.ReadLine(), out int index) && index >= 0)
+            return index;
+        else
+             Console.WriteLine("Некорректный ввод. Введите положительное целое число."); 
+    } while (true);
+}
+
+int[] generate(int size, int rightRange, int leftRange)
+{
+    Random rand = new Random();
+    int[] newArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        newArray[i] = rand.Next(rightRange, leftRange-1);
+    }
+    return newArray;
+}
+
+void print(int[] array, int index)
+{
+    if (index >= 0)
+    {
+        Console.Write(array[index] + " ");
+        print(array, index - 1);
+    }
+}
+
+int size = input("Введите размер массива: ");
+int[] myArray = generate(size, 1, 101);
+Console.Write($"[{string.Join(", ", myArray)}] => ");
+print(myArray, size - 1);
+Console.WriteLine();
